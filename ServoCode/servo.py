6d31376ff92ba474c0ccdccb726366def892a21d
Collin Fueger <collinfueger@yahoo.com>
2018-11-21 18:31:56 -0500

    update objects for a modular object

--- ServoCode/servo.py
+++ ServoCode/servo.py
@@ -2,24 +2,33 @@
 from ServoPi import PWM
 from ServoPi import Servo
 
-def pulseWidthModulation(self):
-    #Pulse Width Modulation(PWM) instatiation
-    pwm = PWM(0x40)
-    #Sets the PWM to a frequency of 200 Hz
-    pwm.set_pwm_freq(200)
-    #Alloww the output to be displayed
-    pwm.output_enable()
-    #Setting the channel for data stream to 1, time period to 1024 out of 4095.
-    pwm.set_pwm(1,0,1024)
+class servoDriver(object):
+    def __init__(self):
+	#creating objects for the PWM and Motor
+        self.pwm = pulseWidthModulation()
+        self.ServoMotor = motor()
 
-def motor(self):
-    #Instantiation of the servo object
-    servo = Servo(0x40)
-    #setting frequency for the PWM to interact with the PWM object
-    servo.set_frequency(50)
-    #setting the servo to move
-    servo.move(1, 125, 250)
+    def pulseWidthModulation(self):
+        #Pulse Width Modulation(PWM) instatiation
+        self.pwm = PWM(0x40)
+        #Sets the PWM to a frequency of 200 Hz
+        self.pwm.set_pwm_freq(200)
+        #Alloww the output to be displayed
+        self.pwm.output_enable()
+        #Setting the channel for data stream to 1, time period to 1024 out of 4095.
+        self.pwm.set_pwm(1,0,1024)
+
+    def motor(self):
+        #Instantiation of the servo object
+        servo = Servo(0x40)
+        #setting frequency for the PWM to interact with the PWM object
+        servo.set_frequency(50)
+        #setting the servo to move
+        servo.move(1, 125, 250)
+
+class RCServo(object):
+    def __init__(self):
+        self.RC_driver = servoDriver()
 
 if name == "main":
-    motor()
-    pulseWidthModulation()
+    RC =  RCServo()
