64353480db260044299bbf00d3166ad8b2712436
cfast20 <coryhalfast@gmail.com>
2019-02-18 22:23:09 -0500

    Updated motor control

--- utils.py
+++ utils.py
@@ -1,56 +1,119 @@
 #Butt ton of imports
 import spidev
+import  time
 import Adafruit_PCA9685
-import CONSTANTS
+import CONSTANTS as c
 
-class motor:
+class Motor:
 
+    currentSpeed = 0
+    currentDir = None
+    obsForward = None
+    obsBehind = None
+    obsLeft = None
+    obsRight = None
     pwm = Adafruit_PCA9685.PCA9685()
 
-    #Functions for darned near everything
-    def accelerate(self, motor1 = False, motor2 = False, speed1 = False, speed2 = False):
+    #Function to accelerate forward
+    def accelerate(self, motor1 = False, motor2 = False, speed1 = c.SERVOMAX, speed2 =c.SERVOMAX):
 
-        for i in range(SERVOMAX):
+        if motor1 is True and motor2 is True and speed2 is False:
 
-            if motor2 is True:
+            for (Motor.currentSpeed) in range(speed1):
 
+                Motor.set_pwm(0, Motor.currentSpeed, 0)
+                Motor.set_pwm(15, -Motor.currentSpeed, 0)
 
+        elif motor1 is True and motor2 is True and speed2 is not False:
 
+            if abs(Motor.currentSpeed - speed1) > abs(Motor.currentSpeed - speed2):
+
+                tempMax = speed1
 
             else:
 
-                #Slowly accelerates the motors to full speed
-                pwm.set_pwm(0, i, 0)
-                pwm.set_pwm(15, -i, 0)
+                tempMax = speed2
 
-    def deccelerate(self, motor1 = False, motor2 = False):
+            for (Motor.currentSpeed) in range(tempMax):
 
+                Motor.set_pwm(0, speed1, 0)
+                Motor.set_pwm(15,speed2, 0)
+
+    #Function to slow down
+    def deccelerate(self, motor1 = False, motor2 = False, speed1 = c.SERVOMIN, speed2 = c.SERVOMIN):
+
+        if motor1 is True and motor2 is False:
+
+            for i in range(Motor.currentSpeed):
+
+                Motor.pwm.set_pwm(0, Motor.currentSpeed - i, 0)
+
+        elif motor2 is True and motor1 is False:
+
+            for i in range(Motor.currentSpeed):
+
+                Motor.pwm.set_pwm(15, Motor.currentSpeed - i, 0)
+
+        elif motor1 and motor2 is True:
+
+            if abs(Motor.currentSpeed - speed1) < abs(Motor.currentSpeed - speed2):
+
+                tempMin = speed1
+
+            else:
+
+                tempMin = speed2
+
+            for i in range(tempMin):
+
+                Motor.pwm.set_pwm(0, Motor.currentSpeed - i, 0)
+                Motor.pwm.set_pwm(15, Motor.currentSpeed - i, 0)
+
+        else:
+
+            for i in range(c.SERVOMIN):
+
+                Motor.pwm.set_pwm(0, Motor.currentSpeed - i, 0)
+                Motor.pwm.set_pwm(15, Motor.currentSpeed - i, 0)
+
+    #Function to turn the car left
     def turnLeft(self, hardness = 0):
 
+    #Function to turn the car right
     def turnRight(self, hardness = 0):
 
+    #Function to stop the car
     def stop(self, motor1 = False, motor2 = False):
 
         if motor1 and motor2 is True:
 
-            pwm.set_all_pwm(0, 0)
+            Motor.pwm.set_all_pwm(0, 0)
 
         elif motor1 is True and motor2 is False:
 
-            pwm.set_pwm(0, 0, 0)
+            Motor.pwm.set_pwm(0, 0, 0)
 
         elif motor1 is False and motor2 is True:
 
-            pwm.set_pwm(15, 0, 0)
+            Motor.pwm.set_pwm(15, 0, 0)
 
         else:
 
-            pwm.set_all_pwm(0, 0)
+            Motor.pwm.set_all_pwm(0, 0)
 
+    #Begin going straight back
     def reverse(self):
 
-        motor.stop()
-        motor.deccelerate()
+        Motor.stop()
+        time.sleep(.5)
+        Motor.deccelerate()
+
+    #Begin going straight forward
+    def start(self):
+
+        Motor.stop()
+        time.sleep(.5)
+        Motor.accelerate()
 
 
 def pingSenseHat():
